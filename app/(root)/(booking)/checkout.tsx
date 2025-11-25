7710c9c9f5d76d554ba56164c6e0bb779dce6e42
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-11-25 15:00:09 +0700

    fix androi layout

--- app/(root)/(booking)/checkout.tsx
+++ app/(root)/(booking)/checkout.tsx
@@ -734,7 +734,7 @@ const Checkout = () => {
                     </View>
                 </View>
             </ScrollView>
-            <View className="absolute bottom-0 left-0 right-0 p-4 bg-white border-t border-gray-200 shadow-xl">
+            <SafeAreaView className="absolute bottom-0 left-0 right-0 p-4 bg-white border-t border-gray-200 shadow-xl">
                 <TouchableOpacity
                     onPress={handleProcessPayment}
                     disabled={!paymentMethod}
@@ -742,7 +742,7 @@ const Checkout = () => {
                 >
                     <Text className="text-white text-center font-bold text-lg">Tiến hành thanh toán</Text>
                 </TouchableOpacity>
-            </View>
+            </SafeAreaView>
             {/* Deal Selection Modal */}
             <Modal
                 visible={dealModalVisible}
